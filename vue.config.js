1cee2872e73a2413bd2508176956d8cfad6dbe2f
César Ubaldo <Invitado@MacBook-Pro-de-ces.local>
2025-03-16 13:04:25 -0600

    add: publicPath en vue.config.json

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/frontend-bank/'  // Cambia 'frontend-bank' al nombre de tu repositorio
+    : '/'
 })
