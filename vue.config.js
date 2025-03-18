2744db22fb3c8e77460fdc42b8d2b0c09c91c569
César Ubaldo <Invitado@MacBook-Pro-de-ces.local>
2025-03-17 18:51:34 -0600

    borrar archivos innecesarios

--- vue.config.js
+++
@@ -1,8 +0,0 @@
-const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath:
-    process.env.NODE_ENV === "production"
-      ? "/frontend-bank/" // Cambia 'frontend-bank' al nombre de tu repositorio
-      : "/",
-});
